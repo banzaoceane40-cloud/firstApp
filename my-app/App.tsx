dc2ea10a815d52109c20d67a515569fe263c6bfd
banzaoceane40-cloud <banzaoceane40@gmail.com>
2026-07-23 16:14:46 +0200

    page styled

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to my app!</Text>
+      <Text>Enter your name</Text>
+      <TextInput placeholder="Oceane"/>
+      <Text>Enter your surname</Text>
+      <TextInput placeholder="Banza"/>
+      <Button title="Add user"/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  welcomeTxt: {
+    paddingTop: 50,
+    color: "green",
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+
   },
+  headingTxt: {
+    fontWeight: "bold",
+  },
+  
 });
